45e254447b50f7dc4d8bf0684c56b7a2e3fd5beb
Ahmed Shafique <2021ee313@student.uet.edu.pk>
2023-09-25 14:37:50 +0500

    data fram row

--- pandas8.py
+++ pandas8.py
@@ -1,1 +1,12 @@
 import pandas as pd
+dataset = {
+    'calc': [5,8,6],
+    'vit': [10, 6, 8]
+}
+
+var = pd.DataFrame(dataset, index= ['day1','day2','day3'])
+print(var)
+
+# printing row
+print(var.loc['day2'])
+
